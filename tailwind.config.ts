7157376ddb4414239cc9b853f8a35c6011e4145e
Filip Tomes <filiptomes@protonmail.com>
2024-03-19 17:58:20 +0100

    style: remove unused code, add color scheme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,12 +7,12 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    colors: {
+      snowWhite: '#F9FFFF',
+      gunmetalBlack: '#183032',
+      tomatoPink: '#FB6C4A',
+      orange: '#F3A503',
+      green: '#009B72',
     },
   },
   plugins: [],

3243ffb77fa7a7955439979641d8622ad32fab4a
Filip Tomes <filiptomes@protonmail.com>
2024-03-23 13:19:35 +0100

    setup: define redirects constant

---
+++ src/lib/constants.ts
@@ -0,0 +1,8 @@
+export const redirects = {
+  toLogin: '/login',
+  toSignup: '/signup',
+  afterLogin: '/dashboard',
+  afterLogout: '/',
+  toVerify: '/verify-email',
+  afterVerify: '/dashboard',
+} as const;

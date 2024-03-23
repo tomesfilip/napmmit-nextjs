2f0b9f44b932ab18ea0d2c12daca8b0a8bdad1f6
Filip Tomes <filiptomes@protonmail.com>
2024-03-23 17:49:46 +0100

    signup schema

--- src/lib/validators/auth.ts
+++ src/lib/validators/auth.ts
@@ -8,3 +8,20 @@ export const loginSchema = z.object({
     .max(255),
 });
 export type LoginInput = z.infer<typeof loginSchema>;
+
+export const signupSchema = z.object({
+  email: z.string().email('Please enter a valid email'),
+  password: z.string().min(1, 'Please provide your password.').max(255),
+});
+export type SignupInput = z.infer<typeof signupSchema>;
+
+export const forgotPasswordSchema = z.object({
+  email: z.string().email(),
+});
+export type ForgotPasswordInput = z.infer<typeof forgotPasswordSchema>;
+
+export const resetPasswordSchema = z.object({
+  token: z.string().min(1, 'Invalid token'),
+  password: z.string().min(8, 'Password is too short').max(255),
+});
+export type ResetPasswordInput = z.infer<typeof resetPasswordSchema>;

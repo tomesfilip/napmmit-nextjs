12c3d7f6186bb253a2920c19fe7d908181ffa7e6
Filip Tomes <filiptomes@protonmail.com>
2024-03-23 17:30:28 +0100

    style: form components

---
+++ src/components/form/PasswordInput.tsx
@@ -0,0 +1,38 @@
+'user client';
+
+import { cn } from '@/lib/utils';
+import { forwardRef, useState } from 'react';
+import { Button } from '../ui/button';
+import { Input, type InputProps } from '../ui/input';
+
+export const PasswordInputComponent = forwardRef<HTMLInputElement, InputProps>(
+  ({ className, ...props }, ref) => {
+    const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+
+    return (
+      <div className="relative">
+        <Input
+          type={isPasswordVisible ? 'text' : 'password'}
+          className={cn('pr-10', className)}
+          ref={ref}
+          {...props}
+        />
+        <Button
+          type="button"
+          variant="ghost"
+          size="sm"
+          className="absolute right-0 top-0 h-full px-3 py-2 hover:bg-transparent"
+          onClick={() => setIsPasswordVisible((prev) => !prev)}
+          disabled={props.value === '' || props.disabled}
+        >
+          {isPasswordVisible ? 'Hide password' : 'Show password'}
+          <span className="sr-only">
+            {isPasswordVisible ? 'Hide password' : 'Show password'}
+          </span>
+        </Button>
+      </div>
+    );
+  },
+);
+
+PasswordInputComponent.displayName = 'PasswordInput';

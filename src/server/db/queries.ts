6ceb30a982f035f606e794f2b4cfa00f328276a8
Filip Tomes <filiptomes@protonmail.com>
2024-06-29 18:47:31 +0200

    refactoring: remove delay

--- src/server/db/queries.ts
+++ src/server/db/queries.ts
@@ -3,6 +3,5 @@ import db from './drizzle';
 
 export const getCottages = cache(async () => {
   const data = await db.query.cottages.findMany();
-  await new Promise((resolve) => setTimeout(resolve, 3000));
   return data;
 });

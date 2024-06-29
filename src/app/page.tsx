ae3e2ec939e834d9cdc6fbd8a4c495855de0fa0e
Filip Tomes <filiptomes@protonmail.com>
2024-06-29 18:35:28 +0200

    refactoring: cottage card component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,5 @@
-import { Badge } from '@/components/ui/badge';
-import cottageFallbackImg from '../../public/cottage-fallback.webp';
-
-import { Button } from '@/components/ui/button';
-import {
-  Card,
-  CardContent,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from '@/components/ui/card';
+import { CottageCard } from '@/components/cottageCard';
 import { getCottages } from '@/server/db/queries';
-import Image from 'next/image';
 
 const Home = async () => {
   const cottages = await getCottages();
@@ -19,39 +8,7 @@ const Home = async () => {
     <main>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 py-8 justify-center place-items-center">
         {cottages.map((cottage) => (
-          <Card key={cottage.id} className="w-full h-full max-w-md">
-            <CardHeader>
-              <CardTitle className="line-clamp-1 leading-8">
-                {cottage.name}
-              </CardTitle>
-            </CardHeader>
-            <CardContent>
-              <div className="flex gap-6">
-                <Image
-                  src={cottageFallbackImg}
-                  alt={`image ${cottage.name}`}
-                  placeholder="blur"
-                  className="w-40 h-full object-cover rounded-lg"
-                />
-                <div className="space-y-3">
-                  <p>{cottage.location}</p>
-                  <p>
-                    Voľné miesta: {cottage.availableBeds}/{cottage.totalBeds}
-                  </p>
-                </div>
-              </div>
-            </CardContent>
-            <CardFooter className="flex w-full justify-between">
-              <div className="flex gap-2">
-                {cottage.hasBreakfast && (
-                  <Badge variant="secondary">raňajky</Badge>
-                )}
-                {cottage.hasDinner && <Badge variant="secondary">večera</Badge>}
-                {cottage.hasShower && <Badge variant="secondary">sprcha</Badge>}
-              </div>
-              <Button className="ml-auto">Viac</Button>
-            </CardFooter>
-          </Card>
+          <CottageCard key={cottage.id} cottage={cottage} />
         ))}
       </div>
     </main>

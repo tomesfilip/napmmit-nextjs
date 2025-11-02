6bfccbb514ae983ac663d70a0dab2a611a09a2d8
Filip Tomes <60915718+tomesfilip@users.noreply.github.com>
2025-11-02 12:10:48 +0000

    add: update packages, add mdx page support

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,4 @@
+import createMDX from '@next/mdx';
 import createNextIntlPlugin from 'next-intl/plugin';
 
 /** @type {import('next').NextConfig} */
@@ -14,7 +15,12 @@ const nextConfig = {
       },
     ],
   },
+  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
 };
 
+const withMDX = createMDX({
+  extension: /\.(md|mdx)$/,
+});
+
 const withNextIntl = createNextIntlPlugin();
-export default withNextIntl(nextConfig);
+export default withNextIntl(withMDX(nextConfig));
